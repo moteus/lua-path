8a5558c45dd60e8f53e625fc626954782a94a9ce
Alexey Melnichuk <mimir@newmail.ru>
2015-12-14 11:22:36 +0300

    Add. `walk_empy_dirs` example.

---
+++ examples/walk_empy_dirs.lua
@@ -0,0 +1,19 @@
+local path = require "path"
+
+local function is_empty_dir(p)
+  local ok = path.each(path.join(p, '*.*'), function()
+    return true
+  end)
+  return not ok
+end
+
+local function walk_empty_dirs(p, fn)
+  local is_recurse = p:sub(1,1) == '!'
+  if is_recurse then p = p:sub(2) end
+
+  path.each(path.join(p, '*.*'), fn, {
+    skipfiles=true, skipdirs=false, recurse=is_recurse, filter=is_empty_dir,
+  })
+end
+
+return walk_empty_dirs
